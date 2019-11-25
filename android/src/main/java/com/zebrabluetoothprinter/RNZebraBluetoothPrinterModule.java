ec91edc8d0e32201b52de89182c3294d616abb6d
Anmol Jain <anmol@geekyants.com>
2019-11-25 19:07:49 +0530

    added dependencies for accessing bluetooth in android

--- android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
+++ android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
@@ -1,11 +1,32 @@
 
 package com.zebrabluetoothprinter;
 
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.widget.Toast;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.util.Log;
+import com.facebook.react.bridge.*;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class RNZebraBluetoothPrinterModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
