e1e6eed3ce91fa4c8f854c644d893e91ee00e81f
Anmol Jain <anmol@geekyants.com>
2019-12-09 15:15:47 +0530

    print feature fixes android

--- android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
+++ android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
@@ -386,56 +386,17 @@ public class RNZebraBluetoothPrinterModule extends ReactContextBaseJavaModule im
     }
   }
   
-  private byte[] getConfigLabel(ZebraPrinter printer, String label, Boolean setTemplate) {
+  private byte[] getConfigLabel(ZebraPrinter printer, String label) {
     byte[] configLabel = null;
-    String template = "CT~~CD,~CC^~CT~\n" + "^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR6,6~SD15^JUS^LRN^CI0^XZ\n"
-        + "^XA\n" + "^MMT\n" + "^PW734\n" + "^LL1231\n" + "^LS0\n" + "^FO0,768^GFA,38272,38272,00092,:Z64:\n"
-        + "eJzs0aERADAIADEG6/AM1rvWIjAIXH6AmI+QJEmSVHtrXXZj59rJw2az2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZo/sDwAA///t3SEBAAAAwrD+rQmARrEHWIWz2Ww2m81ms7/s5ced3bYkSZK+CxLzLNk=:2676\n"
-        + "^FO544,384^GFA,08448,08448,00024,:Z64:\n"
-        + "eJzt2bENACAMA8F0LMVwDI4EC6SJFKjuy2s8gCOk/sbJ2n2+stXJOeecc84555xzzjnnnHPOOa/4419P+tkF6t+Orw==:613B\n"
-        + "^FO0,384^GFA,03072,03072,00024,:Z64:\n"
-        + "eJxjYBgFo4B88B8reEA18QfYLGUcFR8VHxUfFR8VHxVHFad1fTQKRsFwAgDs9Soi:881A\n"
-        + "^FO0,64^GFA,11776,11776,00092,:Z64:\n"
-        + "eJzt2kENACAQA8HzrxIJOAAB8LqkISGzAubTb6skdVqxJvtij9iSbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2W/s5NeCfdqSJP3SBqioe54=:6F90\n"
-        + "^FO160,384^GFA,06656,06656,00052,:Z64:\n"
-        + "eJzt16ENACAMRNEyAftvxyZgcU1NQ8j7/omTFyFJnzZ2refNKs2fDMMwDMMwDMMwTGK6/lyTkSTp6gCtY7SA:79AF\n"
-        + "^FO160,160^GFA,13312,13312,00052,:Z64:\n"
-        + "eJzt2rENACAMA8F0rM3I2QDECGkiBPf9Fe4dIZ1WrXm7ydL6wTAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzAMwzBfm66/f5d5qw1iM9Ft:54B5\n"
-        + "^FO0,160^GFA,23552,23552,00092,:Z64:\n"
-        + "eJzt3DENACAUQ8EvDP86EEICAmAiKSz3BNzSvVWSbpqxBvtg99iSjc1ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2Ww2m81ms9lsNpvNZn+wk3+m7N3W2xZx7Nny:39B6\n"
-        + "^FO0,448^GFA,13824,13824,00072,:Z64:\n"
-        + "eJzt2jENADAMA8EwKH+WZdBS6OBIVXQP4AbPrpIkSertJBrs7MDGi8PhcDgcDofD4XA4HA6Hw+FwOBwOh8PhcDgcDofD4XA4HA6Hw3l0Uj/Yqc5vXbfZvV8=:185D\n"
-        + "^FO0,608^GFA,09216,09216,00072,:Z64:\n"
-        + "eJzt2DEVACAMQ8Fu2EYqTkBEs9B3X8ANGVMlSZK63UR7rnMCGy8Oh8PhcDgcDofD4XA4HA7nUyf1s011JElSvwcBgIlT:9B3D\n"
-        + "^FO0,704^GFA,11776,11776,00092,:Z64:\n"
-        + "eJzt2aERACAQA8Evm7KRODA4sBE/s1fAmshU9WzHWuyPPWNLDjabzWaz2Ww2m81ms9lsNpvNZrPZbDabzWaz2ddOfv/s15YkSZI6dgABKlDe:FE9A\n"
-        + "^FO352,64^GFA,06144,06144,00048,:Z64:\n"
-        + "eJzt10uOnDAQBmBbLFhyBF8kCleaZRZRY2kOkqPEN8gVmBuQnaVBVOovmzYPQyCaTtIS1sht4IP2YLvKrdRVnqxUdLj0l7/8v/WqUc38mKsWrjzuHVxx3NuTXp3zw0nvH+zbB3v7WB9e5+O8//veq7ESP6ga1lbiu7W3ymCWl9G3aOG54t3KDzjiSkdv0cKw1jT2zk+9B0U3G/GDtPDcyqnQtblHDxtUtfheWlglZd6jhzVmlRHfSQvfUuQ9Johx0oZvpYWLOu/xYXBTKd5Jq0teBiH5QWmvaqVwLniyBfvKFhue+3HjR1ot3pbUFvw6yZdZ3/M5h6pT4vmU1+Q09VXWez7+2cvJ4Gu+Qo7HmN9XTRlP9I6qF32TGSSz51uMV3PfYfS5B6ym3sT5ufbcDBW/pUZ9jfO03vItZmOHyorXR7xU1oze3sLMzHosAalc8p8P+y9F8PRR3i28LL89b4TKTXf/6azXf+qxMNQzebx/6n/jWzMbL8zoTZ8Z37Uvd+fDGa+Db+a+2p3/Ex/HN3lZWov1SLRcX83u+t316/hAP1bxgbbjDz/5VeLP9w3f0Ns0vnHw12N8+wDfM3BS3eMt4ifibfJl8gPyBOK5ivEcwZ+zTNOVyZvkOb90yBcvKV/wH+cLOc54jPgkH7VqzK86+XrpnRrzHTJXEfJd3uOBk3zq5Zskn+Z9Jl+HrJn+33eaeNkP4CDuH+ROZHGT94Ps1mULEvcbGJ027CfGSTTx/CiDatw6dbiTr2qal7RfwpX+fjnul9zyp8cT/N65/OX/L3+Vq1zlKtnyC7+HEQk=:C478\n"
-        + "^BY4,3,160^FT50,1068^BCN,,Y,N\n" + "^FD>;8900>60JX>553713012^FS\n" + "^FT31,390^A0N,51,50^FH\\^FDNRHT^FS\n"
-        + "^FT193,455^A0N,28,28^FH\\^FDNN4 5EL^FS\n"
-        + "^FT96,764^A0N,28,28^FH\\^FDStorekeeper instruction: GIVE TO DRIVER^FS\n"
-        + "^FT578,668^A0N,51,50^FH\\^FD72 HR^FS\n" + "^FT94,682^A0N,28,28^FH\\^FDParcel label for ECPABCDEFGH^FS\n"
-        + "^FT33,439^A0N,20,19^FH\\^FDCreation Date: ^FS\n" + "^FT33,463^A0N,20,19^FH\\^FD06-06-2019^FS\n"
-        + "^FT589,556^A0N,102,100^FH\\^FD72^FS\n" + "^FT560,328^A0N,102,100^FH\\^FD02A^FS\n"
-        + "^FT93,517^A0N,25,24^FH\\^FDClick & Collect your online purchases ^FS\n"
-        + "^FT93,548^A0N,25,24^FH\\^FD      to your local Collect+ store^FS\n"
-        + "^FT93,610^A0N,25,24^FH\\^FDwww.collectplus.co.uk/services^FS\n" + "^FT42,303^A0N,102,100^FH\\^FD12^FS\n"
-        + "^FT190,225^A0N,28,28^FH\\^FDJohn Lewis^FS\n" + "^FT190,259^A0N,28,28^FH\\^FDClipper Logistics^FS\n"
-        + "^FT190,293^A0N,28,28^FH\\^FDUnit 1, Saxon Avenue,^FS\n" + "^FT190,327^A0N,28,28^FH\\^FDGrange Park^FS\n"
-        + "^FT190,361^A0N,28,28^FH\\^FDNorthampton^FS\n" + "^FT190,395^A0N,28,28^FH\\^FDNorthamptonshire^FS\n"
-        + "^PQ1,0,1,Y^XZ";
     String printLabel = label;
     try {
       PrinterLanguage printerLanguage = printer.getPrinterControlLanguage();
       SGD.SET("device.languages", "zpl", connection);
 
       if (printerLanguage == PrinterLanguage.ZPL) {
-        if (setTemplate == true) {
-          configLabel = template.getBytes();
-
-        } else {
+       
           configLabel = printLabel.getBytes();
-        }
+        
       } else if (printerLanguage == PrinterLanguage.CPCL) {
         String cpclConfigLabel = "! 0 200 200 406 1\r\n" + "ON-FEED IGNORE\r\n" + "BOX 20 20 380 380 8\r\n"
             + "T 0 6 137 177 TEST\r\n" + "PRINT\r\n";
@@ -448,7 +409,7 @@ public class RNZebraBluetoothPrinterModule extends ReactContextBaseJavaModule im
     return configLabel;
   }
   @ReactMethod
-  public void print(String device, String label, Boolean setTemplate,final Promise promise) {
+  public void print(String device, String label,final Promise promise) {
     boolean success = false;
     boolean loading = false;
     sleep(500);
@@ -472,7 +433,7 @@ public class RNZebraBluetoothPrinterModule extends ReactContextBaseJavaModule im
 
         String pl = SGD.GET("device.languages", connection);
 
-        byte[] configLabel = getConfigLabel(zebraPrinter, label, setTemplate);
+        byte[] configLabel = getConfigLabel(zebraPrinter, label);
         connection.write(configLabel);
         sleep(1500);
         success = true;
